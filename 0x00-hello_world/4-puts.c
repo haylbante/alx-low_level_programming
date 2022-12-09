aab529a68b4a13ee3083fa9643c13c3d03f1c6f4
haylbante <haylbante@gmail.com>
2022-12-09 05:26:32 -0800

    quation4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,31 @@
 # include<stdio.h>
+
+
 /**
- * main -print "Programming is like building a multilingual puzzle, followed by a new line."
- * return 0
- */
-int main (void)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main(void)
 {
-	puts("\" "Programming is like building a multilingual puzzle"
-			return (0);
-			}
+	puts ("\" Programming is like building a multilingual puzzle ");
+
+	    return (0);
+}
+ 
+
+ 
+
+
+			
+		
+			
+			 
+			 
+			 
+			    
+			
+			
+			  
+			 
