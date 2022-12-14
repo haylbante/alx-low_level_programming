e6c564ed2606dc3a280ee4e901a42f4f3d6f41f2
haylbante <haylbante@gmail.com>
2022-12-14 01:19:26 -0800

    0x02. C - Functions, nested loops Projects

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
+ * print_alphabet - in lower case
+ * Return 0
+ */
 void print_alphabet(void)
 {
-	char c;
+	char i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
-
 	_putchar('\n');
 }
-
 
