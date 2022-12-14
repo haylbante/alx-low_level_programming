02aaf8a29a405bd4093c3eabcda93068db456781
haylbante <haylbante@gmail.com>
2022-12-14 01:40:10 -0800

    0x02. C - Functions, nested loops Projects

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- *  * main - the driver function print _putchar followed on a new line
- *   * Return: always 0
- *    */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
