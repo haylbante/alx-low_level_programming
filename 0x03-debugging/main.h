ae2b9d6666226071731a8f982a1e8dbd320d243d
haylbante <haylbante@gmail.com>
2022-12-15 03:19:19 -0800

    Multiple mains

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+int covert_day(int month, int year);
+void print_time_table(int n);
+#endif  
