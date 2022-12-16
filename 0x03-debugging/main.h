f2426ced8b3abd0405543ac660b5f742476c6a47
haylbante <haylbante@gmail.com>
2022-12-16 02:29:33 -0800

    Finializing debuging project

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-int largest_number(int a, int b, int c);
+
 void positive_or_negative(int i);
-int covert_day(int month, int year);
-void print_time_table(int n);
-#endif  
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */ 
